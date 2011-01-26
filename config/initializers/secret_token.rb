c6e3823daa82ff8551472b5db8ac1bbb10555efb
sage <sage@sage-VirtualBox.(none)>
2011-01-26 13:50:55 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2dba9e2c51ff7521814bfb08d4c33dbd31dfd590d18c0cfb9e1fdcf04fff5d160f3ed1676d5c7fbd879ad91da7b6a3477efe73404bb156667ec5e12463c351ae'
